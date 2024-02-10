54c0c9e89cb8f095df70ea583dab21d5c0c5c970
Srinihas <115796290+Srinihas@users.noreply.github.com>
2024-02-10 14:46:22 +0400

    Update take_screenshot.py

--- take_screenshot.py
+++ take_screenshot.py
@@ -1,6 +1,5 @@
 import numpy as np
 import pyautogui
-import imutils
 import cv2
 import mediapipe as mp
 
@@ -13,25 +12,24 @@ finger_tips =[8, 12, 16, 20]
 thumb_tip= 4
 
 while True:
-  ret,img = cap.read()
+  ret, img = cap.read()
   img = cv2.flip(img, 1)
-  h,w,c = img.shape
+  h, w, r = img.shape
   results = hands.process(img)
 
 
   if results.multi_hand_landmarks:
     for hand_landmark in results.multi_hand_landmarks:
       #accessing the landmarks by their position
-      lm_list=[]
+      lm_list = []
       for id ,lm in enumerate(hand_landmark.landmark):
         lm_list.append(lm)
-
-            #array to hold true or false if finger is folded    
+        #array to hold true or false if finger is folded    
       finger_fold_status =[]
       for tip in finger_tips:
         #getting the landmark tip position and drawing blue circle
-          x,y = int(lm_list[tip].x*w), int(lm_list[tip].y*h)
-          cv2.circle(img, (x,y), 15, (255, 0, 0), cv2.FILLED)
+          x, y = int(lm_list[tip].x * w), int(lm_list[tip].y * h)
+          cv2.circle(img, (x, y), 15, (255, 0, 0), cv2.FILLED)
 
         #writing condition to check if finger is folded i.e checking if finger tip starting value is smaller than finger starting position which is inner landmark. for index finger    
         #if finger folded changing color to green
@@ -48,10 +46,16 @@ while True:
         # WRITE THE CODE HERE
         image = pyautogui.screenshot()
         image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
-        cv2.imwrite("in_memory_to_disk", image)
+        cv2.imwrite("in_memory_to_disk.png", image)
         mp_draw.draw_landmarks(img, hand_landmark,
         mp_hands.HAND_CONNECTIONS, mp_draw.DrawingSpec((0,0,255),2,2),
         mp_draw.DrawingSpec((0,255,0),4,2))
 
   cv2.imshow("hand tracking", img)
   cv2.waitKey(1)
+
+  key = cv2.waitKey(1)
+  if key == 32:
+    break
+
+cv2.destroyAllWindows()
